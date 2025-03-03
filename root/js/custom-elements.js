717be0c1c00645516805bfde962249f34d283cba
Josue <josue.orofre@gmail.com>
2025-03-03 14:28:43 -0800

    updated header and footer, started resources and bio page

--- root/js/custom-elements.js
+++ root/js/custom-elements.js
@@ -9,7 +9,7 @@ class NavTag extends HTMLElement {
         this.innerHTML = `
         <nav class="navbar navbar-expand-lg">
     <div class="container">
-      <a class="navbar-brand d-flex align-items-center" href="#">
+      <a class="navbar-brand d-flex align-items-center" href="index.html">
         <img src="../images/leaf.png" alt="Logo" width="35" class="d-inline-block align-text-top">
         grammerstud
       </a>
@@ -27,7 +27,7 @@ class NavTag extends HTMLElement {
           <li class="nav-item">
             <a class="nav-link" href="#">Syllabus</a>
           </li>
-          <li class="nav-item"><a href="#" class="nav-link">Bio</a></li>
+          <li class="nav-item"><a href="bio.html" class="nav-link">Bio</a></li>
         </ul>
       </div>
     </div>
@@ -43,8 +43,10 @@ class NavTag extends HTMLElement {
         links.forEach(link => {
             if (link.getAttribute("href") === currentPage) {
                 link.classList.add("active");
+                link.classList.remove("text-secondary-emphasis");
             } else {
                 link.classList.remove("active");
+                link.classList.add("text-secondary-emphasis");
             }
         });
     }
@@ -63,18 +65,41 @@ class FooterTag extends HTMLElement {
   }
 
   connectedCallback() {
-      this.innerHTML = `
-      <footer class="navbar sticky-bottom py-3 mt-4">
-          <div class="container d-flex flex-column align-items-center">
-              <p class="mb-1">&copy; 2025 Professor Goodwin. All rights reserved.</p>
-              <ul class="nav nav-underline d-flex gap-3">
-                <li class="nav-item"><a class="nav-link text-secondary" href="index.html">Home</a></li>
-                <li class="nav-item"><a class="nav-link text-secondary" href="resources.html">Resources</a></li>
-                <li class="nav-item"><a class="nav-link text-secondary" href="#">Syllabus</a></li>
-                <li class="nav-item"><a class="nav-link text-secondary" href="#">Bio</a></li>
-            </ul>
+      this.innerHTML = `    
+      
+      <div class="navbar sticky-bottom py-3 mt-4">
+        <div class="container d-flex flex-column align-items-center">
+
+          <!-- Copyright -->
+          <p class="mb-2 text-center">&copy; 2025 Professor Goodwin. All rights reserved.</p>
+
+          <!-- Contact Information -->
+          <div class="footer-contact text-center mb-2">
+            <a href="mailto:nicholas.goodwin@sjcc.edu" class="text-secondary">nicholas.goodwin@sjcc.edu</a> |
+            <a href="mailto:ngoodwin@ohlone.edu" class="text-secondary">ngoodwin@ohlone.edu</a> |
+            <a href="tel:16692442399" class="text-secondary">(669) 244-2399</a> |
+            <a href="https://linkedin.com/in/poetemaudit" target="_blank" class="text-secondary">linkedin</a>
           </div>
-      </footer>
+          <!-- Social Media Links -->
+          <div class="footer-socials text-center mb-2">
+            <a href="https://x.com/grammarstud" target="_blank" class="text-secondary">Twitter</a> |
+            <a href="https://x.com/grammarstud" target="_blank" class="text-secondary">Instagram</a>
+          </div>
+          
+          <div class="footer-coffee text-center mb-2">
+          <p>Buy me a coffee?</p>
+            <img src="../images/coffee-qr.png" alt="Buy me a coffee" width="200">
+          </div>
+
+          <!-- Navigation Links -->
+          <ul class="nav nav-underline d-flex gap-3">
+            <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="resources.html">Resources</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Syllabus</a></li>
+            <li class="nav-item"><a class="nav-link" href="bio.html">Bio</a></li>
+          </ul>
+        </div>
+      </div>
       `;
       this.highlightActiveLink();
   }
@@ -86,8 +111,10 @@ class FooterTag extends HTMLElement {
     links.forEach(link => {
         if (link.getAttribute("href") === currentPage) {
             link.classList.add("active");
+            link.classList.remove("text-secondary-emphasis");
         } else {
             link.classList.remove("active");
+            link.classList.add("text-secondary-emphasis");
         }
     });
 }
